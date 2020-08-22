50ec5287cbdeed2486fbddf2cc861a8c923bf6cf
Vasilii Rogin <vasilii@giosg.com>
2020-08-22 21:02:03 +0300

    Working firmware

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -21,23 +21,81 @@ void setup()
     {
         myservo[i].write(90, 100);
     };
+
+    Serial.setTimeout(3000);
+
+    Serial.print("READY\n");
+}
+
+int charToVal(char c)
+{
+    if (c >= '0' && c <= '9')
+    {
+        return c - '0';
+    }
+    else if (c >= 'a' && c <= 'f')
+    {
+        return c - 'a' + 0xA;
+    }
+    else if (c >= 'A' && c <= 'F')
+    {
+        return c - 'A' + 0xA;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 void loop()
 {
-    Serial.print("A1\n");
+    //Serial.print("What do you want?\n");
 
-    myservo[0].write(90 - 60, 100);
-    Serial.print("A2\n");
+    if (Serial.available() > 0)
+    {
+        String str = Serial.readStringUntil('\n');
+        // Serial.print("You wrote: ");
+        // Serial.print(str);
+        // Serial.print("\n");
 
-    myservo[0].wait();
-    Serial.print("A3\n");
+        // M5a64--------------------
+        // M1e64--------------------
+        // M9604--------------------
 
-    Serial.print("A4\n");
+        if (str[0] == 'M' && str.length() == (1 + SERVO_COUNT * 4))
+        {
 
-    myservo[0].write(90 + 60, 100);
-    Serial.print("A5\n");
+            for (int servo_id = 0; servo_id < SERVO_COUNT; servo_id++)
+            {
+                int offset = servo_id * 4 + 1;
 
-    myservo[0].wait();
-    Serial.print("A6\n");
+                if (str[offset] == '-')
+                {
+                    continue;
+                };
+                int degree = charToVal(str[offset]) * 16 + charToVal(str[offset + 1]);
+                int speed = charToVal(str[offset + 2]) * 16 + charToVal(str[offset + 3]);
+                /*
+                Serial.print("Servo ");
+                Serial.print(servo_id);
+                Serial.print(" degree=");
+                Serial.print(degree);
+                Serial.print(" speed=");
+                Serial.print(speed);
+                Serial.print("\n");
+                */
+                myservo[servo_id].write(degree, speed);
+            };
+
+            for (int i = 0; i < SERVO_COUNT; i++)
+            {
+                myservo[i].wait();
+            };
+            Serial.print("OK\n");
+        }
+        else
+        {
+            Serial.print("ERR: Unknown command\n");
+        };
+    };
 }
